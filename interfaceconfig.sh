d83aefcd35b3258acc528f3f09834a7d5b9cf76b
sonebu <buraksoner92@gmail.com>
2024-06-16 18:35:59 +0300

    automatically set up mon0 for the RSS logger computer

--- interfaceconfig.sh
+++ interfaceconfig.sh
@@ -1,5 +1,7 @@
 # turn off wifi and disconnect all networks first
-sudo iw phy phy0 interface add mon0 type monitor
-sudo ip link set mon0 up
+nmcli radio wifi off # turns wifi interface off + gives an rfkill signal
+rfkill unblock wlan  # turns of the rfkill signal without changing wifi interface state
+sudo iw phy phy0 interface add mon0 type monitor # adds monitor mode interface to the network build
+sudo ip link set mon0 up # turns the monitor mode interface on
 
-# this needs work
+# wireshark should be ready to go at this point
