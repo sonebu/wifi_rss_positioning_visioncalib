c24f942e38a3350bc3ff44eadc43962383366203
kadirbilge764 <98549046+kadirbilge764@users.noreply.github.com>
2024-09-02 15:54:07 +0300

    Burakdev (#12)
    * gitignoring data-tshark/ folders
    
    * dev000 (not finished): simple example for manually-built fingerprint dictionary for nearest neighbour positioning
    
    * numerical evaluation of NeNe algos
    
    ---------
    
    Co-authored-by: sonebu <buraksoner92@gmail.com>

--- development/algorithms.py
+++ development/algorithms.py
@@ -1,52 +1,53 @@
- 
-import math
-import torch
+from sklearn.cluster import KMeans 
+import math, torch
 import torch.optim as optim
 import torch.nn as nn
 import numpy as np
 
-from auxiliary import data_dictionary  # auxiliary.py'de tanımlı olan data_dictionary kullanılacak
-from auxiliary import load_data, prepare_data_loaders# auxiliary.py'de tanımlı olan load_data, prepare_data_loaders ve MLP kullanılacak
+#from auxiliary import load_data, prepare_data_loaders# auxiliary.py'de tanımlı olan load_data, prepare_data_loaders ve MLP kullanılacak
 
-#1- nearest neighbour az pt            ||    
-#2- nearest neighbour çok pt           || --> bu metodlar için şu commit ID'de implementation var: f3a2db49692c0928cd671d593489ffb04ea6e9ba     
+#################################################################################################################################################################
+### Nearest Neighbour
+###
 
-def RssPositioningAlgo_NearestNeighbour(rss_values, data_dictionary):
+## inference function (predicts loc based on rss input)
+def RssPositioningAlgo_NearestNeighbour(rss_values, db): # db is a Tuple of the form (<rssi array>,<loc array>)
     min_distance = float('inf')
-    best_match = None
+    best_match_xy = None
 
-    for position, fingerprint in data_dictionary.items():
-        distance = math.dist(rss_values, fingerprint)
+    # check if the length of data lists in db matches those submitted for prediction
+    if(len(db[0][0]) != len(rss_values)):
+        raise ValueError(f'Length of the submitted RSSI array ({len(rss_values)}) for prediction does not match the length of that in the database ({len(db[0][0])})')
+
+    # exhaustive search within db, iterate over rssi elements and extract corresponding locs
+    for fp_idx, fp in enumerate(db[0]):
+        fingerprint  = fp
+        ref_location = db[1][fp_idx]
+        distance     = math.dist(rss_values, fingerprint)
         if distance < min_distance:
             min_distance = distance
-            best_match = position
-            
-    x, y = map(float, best_match.split('_'))
-    return [x,y]
+            best_match_xy   = ref_location
+    return best_match_xy # best_match_xy is an array of size 1x2, for loc_x and loc_y respectively
 
-
-
-#3- nearest neighbour çok pt + interp  --> bunun implementation'ı github'da yok şu anda (1/r^2 model kullanılmıştı)
-
-def RssPositioningAlgo_Interpolation(rss_values, data_dictionary, k=3): # k=3 default olarak verildi
-    distances = []
-    
-    for position, fingerprint in data_dictionary.items(): # data_dictionary'deki her bir eleman için
+## inference function (predicts loc based on rss input), uses interpolation on top of nearest neighbour
+def RssPositioningAlgo_NearestNeighbour_Interpolation(rss_values, db, interp_k=3): # k=3 default olarak verildi
+    distances = []    
+    for fp_idx, fp in enumerate(db[0]): # data_dictionary'deki her bir eleman için
+        fingerprint  = fp
+        ref_location = db[1][fp_idx]
         distance = math.dist(rss_values, fingerprint) # rss_values ve fingerprint arasındaki mesafeyi hesapla
-        distances.append((distance, position)) # mesafe ve position'ı distances listesine ekle
-    
+        distances.append((distance, ref_location)) # mesafe ve ref_location'ı distances listesine ekle
     
     distances.sort(key=lambda x: x[0]) # distances listesini mesafeye göre sırala
-    nearest_neighbors = distances[:k] # en yakın k elemanı nearest_neighbors listesine ekle
-    
+    nearest_neighbors = distances[:interp_k] # en yakın interp_k elemanı nearest_neighbors listesine ekle
    
     weighted_sum_x = 0 # x değerlerinin ağırlıklı toplamı
     weighted_sum_y = 0 # y değerlerinin ağırlıklı toplamı
     total_weight = 0 # toplam ağırlık
     
     for distance, position in nearest_neighbors: # nearest_neighbors listesindeki her bir eleman için
-        weight = 1 / (distance**2) if distance != 0 else 1e10   # ağırlık hesapla
-        x, y = map(float, position.split('_')) # position'ı x ve y'ye ayır
+        weight = 1 / (distance**2) if distance != 0 else 1e10   # ağırlık hesapla (1/r^2 modeli)
+        x, y = position # position'ı x ve y'ye ayır
         weighted_sum_x += weight * x # x değerlerinin ağırlıklı toplamını
         weighted_sum_y += weight * y    # y değerlerinin ağırlıklı toplamını 
         total_weight += weight # toplam ağırlığı güncelle
@@ -55,6 +56,29 @@ def RssPositioningAlgo_Interpolation(rss_values, data_dictionary, k=3): # k=3 de
     estimated_y = weighted_sum_y / total_weight # tahmini y değeri
     
     return (estimated_x, estimated_y) # tahmini x ve y değerlerini döndür
+
+## "training" function (builds database using Kmeans, based on provided training set xt-yt)
+def RssPositioningAlgo_NearestNeighbour_GetKmeansDb(xt, yt, num_clusters, verbose=False):
+    kmeans_rss = KMeans(n_clusters = num_clusters, random_state=0, n_init="auto").fit(yt) 
+    train_cluster_ids = kmeans_rss.labels_
+    if(verbose):
+        print("Cluster IDs of each train set element after Kmeans:\n")
+        print(train_cluster_ids)
+        print("-"*20)
+    db_kmeans_locs = kmeans_rss.cluster_centers_
+    if(verbose):
+        print("Cluster centers (location) of each cluster center (virtual point), {x,y}:\n")
+        print(db_kmeans_locs)
+        print("-"*20)
+    train_cluster_rss_means = np.zeros((num_clusters,3))
+    train_cluster_ctrs      = np.zeros((num_clusters,1)) # to be used for averaging
+    for db_idx, cluster_idx in enumerate(train_cluster_ids):
+        train_cluster_rss_means[cluster_idx] += xt[db_idx].numpy()
+        train_cluster_ctrs[cluster_idx]      += 1
+    db_kmeans_RSSIs = train_cluster_rss_means / train_cluster_ctrs
+    db_kmeans = (db_kmeans_RSSIs, db_kmeans_locs)
+    return db_kmeans
+
 
 #4- MLP-based supervised model
 
--- development/auxiliary.py
+++ development/auxiliary.py
@@ -1,96 +1,123 @@
 # Description: This file contains the data dictionary for the evaluation data.
-import math
-import torch
-import json
+import math, torch, json, random
 import numpy as np
 import torch.utils.data
 import torch.optim as optim
 import torch.nn as nn
-import random
 
-
-def load_data(json_file):
-
-    target_addresses=["d8:84:66:39:29:e8", "d8:84:66:03:61:00", "d8:84:66:4a:06:c9"]
+### filters a data.json file for records from static targets (e.g., APs) which appear together (list of source addresses for static targets = target_addresses)
+### there is a rolling-filter algorithm here with a "second_hold" time which, when it hits one of the target addresses, 
+### it waits for the others to appear as well for a certain amount of time (hold).
+### It drops that record if the others do not appear within the second_hold time
+def loadData_staticTargetAddrMatch(json_file, second_hold = 5, shuffle=False, target_addresses=["d8:84:66:39:29:e8", "d8:84:66:4a:06:c9", "d8:84:66:03:61:00"]): 
     gt_locations = []
     inp_rss_vals = []
     with open(json_file) as f:
         data = json.load(f)
 
-    second_hold = 5
+    # initialization
+    secondold  = None
+    rssi_list  = [None]*len(target_addresses)
+    time_list  = [None]*len(target_addresses)
+    loc_x_list = [None]*len(target_addresses)
+    loc_y_list = [None]*len(target_addresses)
 
-    secondold = None
-
-    rssi1 = None
-    rssi2 = None
-    rssi3 = None
-
-    loc_x = None
-    loc_y= None
-
+    # process each record in the dataset 
     for item in data:
-        
-        timestampnew = item.get("_source", {}).get("layers", {}).get("frame.time", [])[0]
-        secondnew = int(timestampnew.split(":")[-1].split(".")[0])
-        #print(secondnew)
-        loc_x_new = float(item.get("_source", {}).get("layers", {}).get("loc_x", [])[0])
-        loc_y_new = float(item.get("_source", {}).get("layers", {}).get("loc_y", [])[0])
+        timestampnew = item.get("_source", {}).get("layers", {}).get("frame.time", [])[0] # note how only the seconds are extracted
+        secondnew    = int(timestampnew.split(":")[-1].split(".")[0])
+        loc_x_new    = float(item.get("_source", {}).get("layers", {}).get("loc_x", [])[0])
+        loc_y_new    = float(item.get("_source", {}).get("layers", {}).get("loc_y", [])[0])
 
-
-        # Get the source address and check if it's in the target addresses
+        # check if there is a source address, and extract it if there is
         if item.get("_source", {}).get("layers", {}).get("wlan.sa", [])!=[]:
             source_address = item.get("_source", {}).get("layers", {}).get("wlan.sa", [])[0]
         else:
-            continue
+            continue # skip this record if source address does not exist
         
         if secondold is not None :
+            # this is to handle "minute-crossings", i.e., when the new record is in say 01:23:03.25 and old record is in 01:22:59.75
+            # the +=60 is to make the new record later than the old one since the algorithm would fail otherwise. 
             if secondnew < secondold:
                 secondnew += 60  # Adjust for rollover
         
+            # there may be jumps in the recordings due to stop-restarts during experiments
+            # this part filters those out (i.e., if some of the target APs do not appear within second_hold time, the record is dropped)
             if (secondnew-secondold) > second_hold:
-                secondold = None
-                rssi1 = None
-                rssi2 = None
-                rssi3 = None
+                secondold  = None
+                rssi_list  = [None]*len(target_addresses)
+                time_list  = [None]*len(target_addresses)
+                loc_x_list = [None]*len(target_addresses)
+                loc_y_list = [None]*len(target_addresses)
+                continue  # skip this record and reset the data lists
 
-                continue
-
-        # Extract the RSSI value
+        # process the record if it's a valid one (i.e., if it has an src address, and if the time diff is not above the hold period.
         if secondold is None or(secondnew - secondold) <= second_hold:
-            #print("ts is ok")
             if source_address in target_addresses:
-                rssi = float(item.get("_source", {}).get("layers", {}).get("wlan_radio.signal_dbm", [])[0])
-                if source_address == target_addresses[0]:
-                    rssi1 = rssi
-                    #print("rssi1 is ok")
-                elif source_address == target_addresses[2]:
-                    rssi2 = rssi
-                    #print("rssi2 is ok")
-                elif source_address == target_addresses[1]:
-                    rssi3 = rssi
-                    #print("rssi3 is ok")
-            if (rssi1 != None) and (rssi2 != None) and (rssi3 != None):
-                inp_rss_vals.append([rssi1, rssi2, rssi3])  # We may have multiple RSSI values later, so keep it as a list
-                gt_locations.append([loc_x, loc_y])
-                loc_x = loc_x_new
-                loc_y = loc_y_new
+                rssi      = float(item.get("_source", {}).get("layers", {}).get("wlan_radio.signal_dbm", [])[0])
+
+                # get index of the address in the target list so that we can populate that specific element in the data lists 
+                addr_idx  = target_addresses.index(source_address)
+                rssi_list[addr_idx]  = rssi
+                time_list[addr_idx]  = secondnew
+                loc_x_list[addr_idx] = loc_x_new
+                loc_y_list[addr_idx] = loc_y_new
+                
+                ### Note: target address [1] and [2] were swapped earlier ( ["d8:84:66:39:29:e8", "d8:84:66:03:61:00", "d8:84:66:4a:06:c9"] ) 
+                ###       and the if check was [2] = rssi2 and [1] = rssi3, I swapped the address list instead and switched [1] and [2] in the if check
+                #if source_address == target_addresses[0]:
+                #    rssi1 = rssi
+                #elif source_address == target_addresses[1]:
+                #    rssi2 = rssi
+                #elif source_address == target_addresses[2]:
+                #    rssi3 = rssi
+            
+            if(not(any(x is None for x in rssi_list))):
+                inp_rss_vals.append(rssi_list)
+
+                ### design decision: interpolate and get location at the average time point for the list
+                ###                  e.g.,  >>> import numpy as np
+                ###                         >>> time_list=np.asarray([10,12,30])
+                ###                         >>> loc_x_list=np.asarray([1.0,1.5,3.0])
+                ###                         >>> np.interp((time_list[0]+time_list[-1])/2,time_list,loc_x_list)
+                ###                         2.1666666666666665
+                ###                         >>> (time_list[0]+time_list[-1])/2
+                ###                         20.0
+                ###                         >>> 
+                loc_x_interp = np.interp((time_list[0]+time_list[-1])/2, time_list, loc_x_list)
+                loc_y_interp = np.interp((time_list[0]+time_list[-1])/2, time_list, loc_y_list)
+                gt_locations.append([loc_x_interp, loc_y_interp])
+
+                # reset
                 secondold = secondnew % 60
-                rssi1 = None
-                rssi2 = None
-                rssi3 = None
+                rssi_list  = [None]*len(target_addresses)
+                time_list  = [None]*len(target_addresses)
+                loc_x_list = [None]*len(target_addresses)
+                loc_y_list = [None]*len(target_addresses)
+
+            #if (rssi1 != None) and (rssi2 != None) and (rssi3 != None): # ToDo: change this to rssi_list
+            #    inp_rss_vals.append([rssi1, rssi2, rssi3])  # We may have multiple RSSI values later, so keep it as a list
+            #    gt_locations.append([loc_x, loc_y])
+            #    secondold = secondnew % 60
+            #    loc_x = loc_x_new 
+            #    loc_y = loc_y_new
+            #    rssi1 = None
+            #    rssi2 = None
+            #    rssi3 = None
     
-    # Zip the lists together
-    combined = list(zip(inp_rss_vals[1:], gt_locations[1:]))
+    if(shuffle):
+        # Zip the lists together
+        combined = list(zip(inp_rss_vals[1:], gt_locations[1:]))
 
-    # Shuffle the combined list
-    random.shuffle(combined)
+        # Shuffle the combined list
+        random.shuffle(combined)
 
-    # Unzip the combined list back into two separate lists
-    inp_rss_vals_shuffled, gt_locations_shuffled = zip(*combined)
+        # Unzip the combined list back into two separate lists
+        inp_rss_vals_shuffled, gt_locations_shuffled = zip(*combined)
 
-    # Convert back to lists (zip returns tuples)
-    inp_rss_vals = list(inp_rss_vals_shuffled)
-    gt_locations = list(gt_locations_shuffled)
+        # Convert back to lists (zip returns tuples)
+        inp_rss_vals = list(inp_rss_vals_shuffled)
+        gt_locations = list(gt_locations_shuffled)
 
     # Convert lists to numpy arrays
     gt_locations = np.asarray(gt_locations)
@@ -118,24 +145,3 @@ def prepare_data_loaders(inp_rss_vals, gt_locations, batch_size=1, train_test_sp
 
     return train_loader, test_loader, tensor_x_train, tensor_y_train, tensor_x_test, tensor_y_test
 
-
-data_dictionary = {
-    '0_0': [-34, -63, -52],
-    '0_7': [-50, -71, -70],
-    '-6_7': [-45, -66, -66],
-    '6_7': [-42, -65, -66],
-    '-14_2': [-41, -71, -70],
-    '14_2': [-48, -53, -53],
-    '-28_-4': [-62, -71, -44],
-    '-28_4': [-53, -73, -41],
-    '28_-4': [-56, -44, -70],
-    '28_4': [-53, -41, -72],
-    '25_0': [-53, -36, -61],
-    '-25_-2': [-54, -63, -37],
-    '7_-2': [-42, -48, -56],
-}
-
-
-
-
-# placeholder
