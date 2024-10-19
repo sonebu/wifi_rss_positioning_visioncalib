e9d2887f950aa6732ede35c09d0b0daa1bd1287b
erdemergen44 <e.erdemergen@gmail.com>
2024-10-19 20:42:01 +0300

    RealTime WifiScan

---
+++ utilities/wifi_realtime_scan/wifiscan_real_time.py
@@ -0,0 +1,140 @@
+from subprocess import Popen, PIPE
+from matplotlib import pyplot as plt
+import matplotlib.font_manager as font_manager
+import numpy as np
+import json
+
+
+# Tshark command
+command = "tshark -i mon0 -T json -e frame.time -e wlan_radio.signal_dbm -e wlan.sa -e wlan.ssid -e wlan.ra -e wlan.da -e wlan_radio.frequency -e wlan_radio.snr -e wlan_radio.noise_dbm -e wlan.antenna.id"
+
+def filterbySSID():
+    ####### Filter data by unique source addresses and get lists
+    lo_uniqueSAs = []
+    lo_freqs = []
+    lo_SActrs = []
+    lo_SSIDs = []
+
+    for pkt in packets:
+
+        if(pkt["_source"]["layers"]["wlan.sa"] not in lo_uniqueSAs):
+            lo_uniqueSAs.append(pkt["_source"]["layers"]["wlan.sa"])
+            lo_freqs.append(pkt["_source"]["layers"]["wlan_radio.frequency"][0])
+            lo_SActrs.append(1)
+            lo_SSIDs.append(pkt["_source"]["layers"]["wlan.ssid"][0])
+        else:
+            position_of_uniqueSA_in_listofuniqueSAs = lo_uniqueSAs.index(pkt["_source"]["layers"]["wlan.sa"])
+            lo_SActrs[position_of_uniqueSA_in_listofuniqueSAs] += 1
+
+    # Print results
+    print("SSID | Source Address    |  Freq |  # of occurrences")
+    print("-" * 50)
+    KU_AP_idx = []
+
+    for i, ssid in enumerate(lo_SSIDs):
+        if ssid == "KU":
+            print(f"{ssid}  | {lo_uniqueSAs[i]} | {lo_freqs[i]} | {lo_SActrs[i]}")
+            KU_AP_idx.append(i)
+
+
+    ############ Plot the RSS values for each Source        
+
+    def frametime2secs(frametime):
+        hrs   = int(frametime.split(":")[0][-2:])
+        mins  = int(frametime.split(":")[1])
+        secs  = int((frametime.split(":")[2]).split(".")[0])
+        msecs = int((frametime.split(":")[2]).split(".")[1][:1])
+        total_seconds = (hrs*60*60*1000 + mins*60*1000 + secs*1000 + msecs*10) / 1000.0 
+        return total_seconds
+
+    def get_timeinsecs(sampledict, offset=0):
+        return frametime2secs(sampledict["_source"]["layers"]["frame.time"][0]) - offset-7780;
+
+    def get_freq(sampledict):
+        return sampledict["_source"]["layers"]["wlan_radio.frequency"][0]
+
+    def get_sourceaddr(sampledict):
+        return sampledict["_source"]["layers"]["wlan.sa"]
+        
+    def get_ssid(sampledict):
+        return sampledict["_source"]["layers"]["wlan.ssid"][0]
+        
+    def get_rss(sampledict):
+        return sampledict["_source"]["layers"]["wlan_radio.signal_dbm"][0]
+
+    time = []
+    for sample in packets:
+        t = get_timeinsecs(sample, offset=64999.800)
+        if(t not in time):
+            time.append(t)
+    print("Time : " , time[0])
+    array_ssid_rss = np.zeros((len(time), len(KU_AP_idx)))*np.nan
+    for sample in packets:
+        t_sample  = get_timeinsecs(sample, offset=64999.800)
+        t_idx     = time.index(t_sample)
+        sa_sample = get_sourceaddr(sample)
+        sa_idx    = lo_uniqueSAs.index(sa_sample)
+        if(sa_idx in KU_AP_idx):
+            array_ssid_idx = KU_AP_idx.index(sa_idx)
+            array_ssid_rss[t_idx, array_ssid_idx] = get_rss(sample)
+
+    plt.figure(figsize=(16, 5), dpi=80)
+    plt.plot(time, array_ssid_rss, marker="*");
+    plt.ylim([-95, 0])
+    plt.grid()
+    plt.legend([(lo_uniqueSAs[idx][0]+ ", count: " + str(lo_SActrs[idx]) + ", freq: " + str(lo_freqs[idx])) for idx in KU_AP_idx], 
+        prop=font_manager.FontProperties(family='monospace'), loc='upper center', ncol=3)
+    plt.show()
+
+def run(command):
+    process = Popen(command, stdout=PIPE, shell=True)
+    while True:
+        line = process.stdout.readline().rstrip()
+        if not line:
+            break
+        yield line.decode('utf-8')
+
+
+if __name__ == "__main__":
+    packets = []
+    current_packet = ""
+
+    for path in run(command):
+        if not path == "[":
+            current_packet += path.strip()
+            if path.strip() == "},":
+                item = json.loads(current_packet[:-1])
+                frame_time = item.get("_source", {}).get("layers", {}).get("frame.time", [""])[0]
+                wlan_ssid = item.get("_source", {}).get("layers", {}).get("wlan.ssid", [""])[0]
+                transformed_item = {
+                    "_index": item.get("_index", "unknown"),
+                    "_type": item.get("_type", "doc"),
+                    "_score": item.get("_score", None),
+                    "_source": {
+                        "layers": {
+                            "frame.time": [frame_time],
+                            "wlan_radio.signal_dbm": item.get("_source", {}).get("layers", {}).get("wlan_radio.signal_dbm", []),
+                            "wlan.sa": item.get("_source", {}).get("layers", {}).get("wlan.sa", []),
+                            "wlan.ra": item.get("_source", {}).get("layers", {}).get("wlan.ra", []),
+                            "wlan.da": item.get("_source", {}).get("layers", {}).get("wlan.da", []),
+                            "wlan_radio.frequency": item.get("_source", {}).get("layers", {}).get("wlan_radio.frequency", []),
+                            "wlan.ssid": [wlan_ssid]
+                        }
+                    }
+                }         
+
+
+                packets.append(transformed_item)
+                current_packet = ""
+                if len(packets) % 200 == 0:
+                    #for i in packets:
+                     #   print(i,"\n********")
+                    filterbySSID()
+
+
+
+                
+           
+
+         
+        
